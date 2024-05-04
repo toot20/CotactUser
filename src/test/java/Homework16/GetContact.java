c012027be3029c9e1a2d5bcc6630a3b63736ff56
toot20 <atikahalmutlq.CS@gmail.com>
2024-05-04 22:28:25 +0300

    Updated

--- src/test/java/Homework16/GetContact.java
+++ src/test/java/Homework16/GetContact.java
@@ -1,4 +1,38 @@
 package Homework16;
 
-public class GetContact {
+import base_urls.ContactListBaseUrl;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import pojos.ContactPojo;
+
+import static io.restassured.RestAssured.given;
+import static utilities.ObjectMapperUtils.convertJsonToJava;
+
+public class GetContact extends ContactListBaseUrl {
+    @Test
+    void GetContactTest() {
+        //Set the url
+        spec.pathParams("first", "contacts");
+
+        String strJson = """
+                {
+                 "firstName": "Test",
+                 "lastName": "User",
+                 "birthdate": "1970-01-01",
+                 "email": "atikah@fake.com",
+                 "phone": "8005555555",
+                 "street1": "1 Main St.",
+                 "street2": "Apartment A",
+                 "city": "Anytown",
+                 "stateProvince": "KS",
+                 "postalCode": "12345",
+                 "country": "USA"
+                }""";
+
+        ContactPojo expectedData = convertJsonToJava(strJson, ContactPojo.class);
+        System.out.println("expectedData = " + expectedData);
+
+        Response response = given(spec).get("{first}");
+        response.prettyPrint();
+    }
 }
